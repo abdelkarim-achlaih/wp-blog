cf92490de1d059e00f667c1ac0a01f23e41d2d44
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-07-26 02:24:28 +0100

    Updating Footer Colors

--- footer.php
+++ footer.php
@@ -6,12 +6,12 @@
                 <p class="lh-lg">Unleash the potential of your online presence with my web development expertise. Together, we'll create a captivating digital experience that leaves a lasting impact on your audience. Let's build something amazing!</p>
             </div>
             <div class="col-lg-4 col-md-6 mb-4">
-                <h2 class="text-light fw-bold mb-3">Our Articles</h2>
+                <h2 class="fw-bold mb-3">Our Articles</h2>
                 <?php footer_section_2() ?>
             </div>
             <div class="col-lg-4 col-md-6 mb-4">
-                <h2 class="text-light fw-bold">Contact Us</h2>
-                <p class="mb-5 mt-3">Contact us via e-mail or telephone. We look forward to hearing from you</p>
+                <h2 class="fw-bold">Contact Us</h2>
+                <p class="mb-5 mt-3 lh-lg">Contact us via e-mail or telephone. We look forward to hearing from you</p>
                 <a href="mailto:abdelkarima46@gmail.com" class="btn main-btn rounded-pill w-100">abdelkarima46@gmail.com</a>
                 <ul class="social d-flex list-unstyled gap-5 mt-5 justify-content-center justify-content-md-start">
                     <li>
