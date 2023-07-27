40a5c3e771f96e3edc8a5677f91a86a2a75d63b4
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-07-27 15:55:41 +0100

    Filter More Articles in single page

--- single.php
+++ single.php
@@ -82,15 +82,19 @@
             <h3 class="fw-bold text-white fs-4">More Articles</h3>
           </div>
           <?php
-            if (have_posts()) {
-              while (have_posts()) {
-                the_post();
+            $categories = get_the_category(get_the_ID());
+            $args = array(
+              'category__in' => $categories[0]->term_id,
+              'post_type' => 'post',
+              'posts_per_page' => 5,
+              'orderby' => 'rand',
+              'post__not_in' => array(get_the_ID())
+            );
+            $query = new WP_Query($args);
+            if ($query->have_posts()) {
+              while ($query->have_posts()) {
+                $query->the_post();
                 ?>
-                  <div class="card-body mb-3 rounded">
-                    <h5 class="card-title fw-bold lh-base"><?php the_title(); ?></h5>
-                    <?php the_excerpt(); ?>
-                    <a href="<?php the_permalink(); ?>" class="btn rounded-pill">Read more</a>
-                  </div>
                   <div class="card-body mb-3 rounded">
                     <h5 class="card-title fw-bold lh-base"><?php the_title(); ?></h5>
                     <?php the_excerpt(); ?>
