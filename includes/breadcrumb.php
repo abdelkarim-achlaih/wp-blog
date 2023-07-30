e4f9772c5e9d4be7c2fd64685765b8ba03ab3c17
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-07-30 19:42:43 +0100

    Add Breadcrumb Backend

--- includes/breadcrumb.php
+++ includes/breadcrumb.php
@@ -2,9 +2,11 @@
   <div class="container">
     <nav aria-label="breadcrumb">
       <ol class="breadcrumb justify-content-lg-start justify-content-center">
-        <li class="breadcrumb-item"><a href="#">Home</a></li>
-        <li class="breadcrumb-item"><a href="#">Library</a></li>
-        <li class="breadcrumb-item active" aria-current="page">Data</li>
+        <li class="breadcrumb-item"><a href="<?php get_home_url()?>">Home</a></li>
+        <li class="breadcrumb-item">
+          <?php the_category(', '); ?>
+        </li>
+        <li class="breadcrumb-item active" aria-current="page"><?php the_title(); ?></li>
       </ol>
     </nav>
   </div>
