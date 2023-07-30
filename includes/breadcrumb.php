40f2ae57a43006107a71ba3f164403374b5e188d
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-07-30 19:54:06 +0100

    Fix Breadcrumb Responsiveness

--- includes/breadcrumb.php
+++ includes/breadcrumb.php
@@ -1,9 +1,9 @@
 <section class="breadcrumb-holder p-0 m-0 mt-5">
   <div class="container">
     <nav aria-label="breadcrumb">
-      <ol class="breadcrumb justify-content-lg-start justify-content-center">
-        <li class="breadcrumb-item"><a href="<?php get_home_url()?>">Home</a></li>
-        <li class="breadcrumb-item">
+      <ol class="breadcrumb justify-content-lg-start justify-content-center text-center">
+        <li class="breadcrumb-item mb-3"><a href="<?php get_home_url()?>">Home</a></li>
+        <li class="breadcrumb-item mb-3">
           <?php the_category(', '); ?>
         </li>
         <li class="breadcrumb-item active" aria-current="page"><?php the_title(); ?></li>
